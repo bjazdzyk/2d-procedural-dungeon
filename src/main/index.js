65c13bb2e70c5d2928bc104096d91c5777b30619
Bartosz Jażdżyk <bartek.jazdzyk.007@gmail.com>
2022-07-06 12:29:43 +0200

    webpack template

---
+++ src/main/index.js
@@ -0,0 +1,67 @@
+import { app, BrowserWindow } from 'electron'
+import * as path from 'path'
+import { format as formatUrl } from 'url'
+
+const isDevelopment = process.env.NODE_ENV !== 'production'
+
+let mainWindow
+
+function createMainWindow() {
+  const window = new BrowserWindow({
+    width: 1000,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    }
+  })
+
+
+
+  if (isDevelopment) {
+    window.webContents.openDevTools()
+  }
+
+  if (isDevelopment) {
+    window.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`)
+  }
+  else {
+    window.loadURL(formatUrl({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file',
+      slashes: true
+    }))
+  }
+
+  window.on('closed', () => {
+    mainWindow = null
+  })
+
+  window.webContents.on('devtools-opened', () => {
+    window.focus()
+    setImmediate(() => {
+      window.focus()
+    })
+  })
+
+  window.maximize()
+  window.setMenu(null)
+
+  return window
+}
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    mainWindow = createMainWindow()
+  }
+})
+
+
+app.on('ready', () => {
+  mainWindow = createMainWindow()
+})
